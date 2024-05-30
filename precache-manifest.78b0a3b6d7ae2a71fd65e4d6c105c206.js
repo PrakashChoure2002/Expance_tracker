e91ad0fe725923a75122a376bd19af1fd4a23a40
prakash choure <prakashchoure2002@gmail.com>
2024-05-30 14:00:24 +0530

    Updates

---
+++ precache-manifest.78b0a3b6d7ae2a71fd65e4d6c105c206.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec7b8265aa4a5d3e278194b5f7d8f23",
+    "url": "/Expance_tracker/index.html"
+  },
+  {
+    "revision": "f629bda279e82cc8696c",
+    "url": "/Expance_tracker/static/css/main.571e6405.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/Expance_tracker/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Expance_tracker/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f629bda279e82cc8696c",
+    "url": "/Expance_tracker/static/js/main.7ecfcc4f.chunk.js"
+  },
+  {
+    "revision": "fd96768c98e2eb5d3d3d",
+    "url": "/Expance_tracker/static/js/runtime-main.44b96640.js"
+  }
+]);
